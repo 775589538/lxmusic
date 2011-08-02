8e9045254f9b4320fa9ea7ac5eb168674e448866
Shan-Bin Chen (DreamerC) <dreamerwolf.tw@gmail.com>
2011-08-03 00:07:26 +0800

    merge debian/0.4.4+git20100802

--- src/lxmusic.c
+++ src/lxmusic.c
@@ -88,6 +88,7 @@ static void 	send_notification_pixbuf		( LXMusicNotification lxn, GdkPixbuf *pix
 static int 	update_track				( xmmsv_t *value, GtkTreeIter* it );
 static int 	on_coll_info_received			( xmmsv_t* value, void* user_data );
 static int 	on_picture_front_received		( xmmsv_t* value, void* user_data );
+static int 	on_playback_started			( xmmsv_t* value, void* user_data );
 
 static xmmsc_connection_t *con = NULL;
 static GtkWidget *main_win = NULL;
@@ -141,6 +142,9 @@ static int win_ypos = 0;
 
 void 			on_locate_cur_track	(GtkAction* act, gpointer user_data);
 void 			on_play_btn_clicked	(GtkButton* btn, gpointer user_data);
+
+static gboolean 	on_tray_icon_middle_clicked	(GtkWidget *widget, GdkEventButton *event, gpointer data);
+static void 		on_volume_btn_scrolled		(GtkWidget *widget, GdkEventScroll *event, gpointer user_data);
 
 static GtkTreeIter	get_current_track_iter	();
 static gboolean		get_track_properties 	(xmmsv_t *value, TrackProperties *properties);
@@ -445,6 +449,28 @@ static void create_tray_icon()
     gtk_status_icon_set_tooltip(GTK_STATUS_ICON(tray_icon), _("LXMusic"));
     g_signal_connect(tray_icon, "activate", G_CALLBACK(on_tray_icon_activate), NULL );
     g_signal_connect(tray_icon, "popup-menu", G_CALLBACK(on_tray_icon_popup_menu), NULL );
+    g_signal_connect(tray_icon, "scroll-event", G_CALLBACK(on_volume_btn_scrolled), volume_btn);
+    g_signal_connect(tray_icon, "button_press_event", G_CALLBACK(on_tray_icon_middle_clicked), NULL );
+ }
+
+static gboolean on_tray_icon_middle_clicked(GtkWidget *widget, GdkEventButton *event, gpointer data) { 
+    /* Only handle the keypress event on middle click Toggle play/pause */
+    if ( event->button == 2 ) {
+	xmmsc_result_t *res;
+	if( playback_status == XMMS_PLAYBACK_STATUS_PLAY )
+	{
+	    res = xmmsc_playback_pause(con);
+	    xmmsc_result_notifier_set_and_unref(res, on_playback_started, NULL);
+	}
+	else
+	{
+	    res = xmmsc_playback_start(con);
+	    xmmsc_result_unref(res);
+	}
+	return TRUE;
+	
+    } 
+    return FALSE;
 }
 
 void on_preference(GtkAction* act, gpointer data)
